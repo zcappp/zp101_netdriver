42ccbda149bd0c71e4e4cfadce001231bf081b8d
zc <zc-yun@outlook.com>
2023-04-03 20:50:20 +0800

    1

--- plugins/zp101_网盘.js
+++ plugins/zp101_网盘.js
@@ -56,7 +56,7 @@ function rFavorite(ref, o) {
     return <svg onClick={e => favorite(e, o, ref)} className={"zsvg " + (ref.favorites.find(a => a._id === o._id) ? "zp101stared" : "zp101star")} viewBox="64 64 896 896"><path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"/></svg>
 }
 
-function onInit(ref) {
+function init(ref) {
     const { exc, props, render } = ref
     const type = ref.type = props.type || "i"
     ref.auth = exc("$c.me._id")
@@ -122,7 +122,7 @@ function del(e, id, ref) {
     })
 }
 
-function onDestroy({ io }) {
+function destroy({ io }) {
     io && io.disconnect()
 }
 
@@ -151,8 +151,8 @@ $plugin({
         label: "【网盘】文本"
     }],
     render,
-    onInit,
-    onDestroy,
+    init,
+    destroy,
     css
 })
 
